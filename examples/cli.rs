6c1a2e21346a85545eede24b804c46049d00b8dc
gak <gak@gak0.com>
2021-03-12 20:37:49 +1100

    cli example wip

--- examples/cli.rs
+++ examples/cli.rs
@@ -1,5 +1,28 @@
+use anyhow::Result;
+/// This example is to show off how to use the CLI, and also acts as an integration test.
+use clap::Clap;
 use duct::cmd;
+use std::path::PathBuf;
 
-fn main() {
-    cmd!("feeless", "--help").run().unwrap();
+// These Opts are only for the path to feeless, not the actual feeless CLI.
+#[derive(Clap)]
+struct Opts {
+    /// Specify the path to the feeless binary, e.g. "target/Debug/feeless"
+    feeless_path: PathBuf,
+}
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    let bin = &opts.feeless_path;
+
+    assert!(cmd!(bin, "--help").read()?.contains("cryptocurrency"));
+
+    // This is just a sanity check since it is non-deterministic, we can't check the result, only
+    // to see if there is an error.
+    cmd!(bin, "phrase", "random")
+        .pipe(cmd!(bin, "phrase", "-", "public"))
+        .pipe(cmd!(bin, "public", "-", "address"))
+        .run()?;
+
+    Ok(())
 }
