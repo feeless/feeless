fab44dc8a5bfa164c72e242ea8a692664d7ae50f
gak <gak@gak0.com>
2021-03-16 13:01:41 +1100

    cleaner cli unit conversion code using macros

--- src/cli/unit.rs
+++ src/cli/unit.rs
@@ -4,6 +4,30 @@ use crate::Rai;
 use clap::Clap;
 use std::str::FromStr;
 
+macro_rules! rai {
+    ($dst:expr) => {
+        UnboundedRai::from_str(&$dst.resolve()?)?
+    };
+}
+
+macro_rules! nano {
+    ($dst:expr) => {
+        Nano::from_str(&$dst.resolve()?)?
+    };
+}
+
+macro_rules! cents {
+    ($dst:expr) => {
+        Cents::from_str(&$dst.resolve()?)?
+    };
+}
+
+macro_rules! micro {
+    ($dst:expr) => {
+        MicroNano::from_str(&$dst.resolve()?)?
+    };
+}
+
 #[derive(Clap)]
 pub(crate) struct UnitOpts {
     #[clap(subcommand)]
@@ -16,55 +40,31 @@ impl UnitOpts {
         // Not sure how to improve.
         let src = match &self.command {
             SrcUnit::Rai(src) => match &src.dst {
-                DstCommand::Rai(dst) => UnboundedRai::from_str(&dst.resolve()?)?
-                    .to_unbounded_rai()
-                    .to_string(),
-                DstCommand::Nano(dst) => UnboundedRai::from_str(&dst.resolve()?)?
-                    .to_nano()
-                    .to_string(),
-                DstCommand::Cents(dst) => UnboundedRai::from_str(&dst.resolve()?)?
-                    .to_cents()
-                    .to_string(),
-                DstCommand::Micro(dst) => UnboundedRai::from_str(&dst.resolve()?)?
-                    .to_micro_nano()
-                    .to_string(),
+                DstCommand::Rai(dst) => rai!(dst).to_unbounded_rai().to_string(),
+                DstCommand::Nano(dst) => rai!(dst).to_nano().to_string(),
+                DstCommand::Cents(dst) => rai!(dst).to_cents().to_string(),
+                DstCommand::Micro(dst) => rai!(dst).to_micro_nano().to_string(),
             }
             .to_string(),
             SrcUnit::Nano(src) => match &src.dst {
-                DstCommand::Rai(dst) => Nano::from_str(&dst.resolve()?)?
-                    .to_unbounded_rai()
-                    .to_string(),
-                DstCommand::Nano(dst) => Nano::from_str(&dst.resolve()?)?.to_nano().to_string(),
-                DstCommand::Cents(dst) => Nano::from_str(&dst.resolve()?)?.to_cents().to_string(),
-                DstCommand::Micro(dst) => {
-                    Nano::from_str(&dst.resolve()?)?.to_micro_nano().to_string()
-                }
+                DstCommand::Rai(dst) => nano!(dst).to_unbounded_rai().to_string(),
+                DstCommand::Nano(dst) => nano!(dst).to_nano().to_string(),
+                DstCommand::Cents(dst) => nano!(dst).to_cents().to_string(),
+                DstCommand::Micro(dst) => nano!(dst).to_micro_nano().to_string(),
             }
             .to_string(),
             SrcUnit::Cents(src) => match &src.dst {
-                DstCommand::Rai(dst) => Cents::from_str(&dst.resolve()?)?
-                    .to_unbounded_rai()
-                    .to_string(),
-                DstCommand::Nano(dst) => Cents::from_str(&dst.resolve()?)?.to_nano().to_string(),
-                DstCommand::Cents(dst) => Cents::from_str(&dst.resolve()?)?.to_cents().to_string(),
-                DstCommand::Micro(dst) => Cents::from_str(&dst.resolve()?)?
-                    .to_micro_nano()
-                    .to_string(),
+                DstCommand::Rai(dst) => cents!(dst).to_unbounded_rai().to_string(),
+                DstCommand::Nano(dst) => cents!(dst).to_nano().to_string(),
+                DstCommand::Cents(dst) => cents!(dst).to_cents().to_string(),
+                DstCommand::Micro(dst) => cents!(dst).to_micro_nano().to_string(),
             }
             .to_string(),
             SrcUnit::Micro(src) => match &src.dst {
-                DstCommand::Rai(dst) => MicroNano::from_str(&dst.resolve()?)?
-                    .to_unbounded_rai()
-                    .to_string(),
-                DstCommand::Nano(dst) => {
-                    MicroNano::from_str(&dst.resolve()?)?.to_nano().to_string()
-                }
-                DstCommand::Cents(dst) => {
-                    MicroNano::from_str(&dst.resolve()?)?.to_cents().to_string()
-                }
-                DstCommand::Micro(dst) => MicroNano::from_str(&dst.resolve()?)?
-                    .to_micro_nano()
-                    .to_string(),
+                DstCommand::Rai(dst) => micro!(dst).to_unbounded_rai().to_string(),
+                DstCommand::Nano(dst) => micro!(dst).to_nano().to_string(),
+                DstCommand::Cents(dst) => micro!(dst).to_cents().to_string(),
+                DstCommand::Micro(dst) => micro!(dst).to_micro_nano().to_string(),
             },
         };
         println!("{}", src);
