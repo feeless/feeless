70338d8ba44934716ae67c9ce4c90c9a0d5665d7
Kevin Kwon <fsw0422@gmail.com>
2021-03-27 02:19:39 +0100

    Improved comments

--- buildenv/builder.py
+++ buildenv/builder.py
@@ -49,10 +49,10 @@ def build():
 
     if "Linux" in os:
         """
-        In Linux, Docker user IDs are mapped host to container one-to-one by default
-        The problem arises when container starts creating files into directories that are mounted to host
-        This cause the host to have no permission to have write access to the created files
-        It is better to create a user in the container that maps to the same user Id / group Id to the host
+        in Linux, Docker user IDs are mapped host to container one-to-one by default
+        the problem arises when container starts creating files into directories that are mounted to host
+        this cause the host to have no permission to have write access to the created files
+        it is better to create a user in the container that maps to the same user Id / group Id to the host
         """
 
         from os import environ, getgid, getuid
@@ -70,8 +70,8 @@ def build():
     else:
         """
         Mac and Windows users are not running Docker natively and by utilizing transparent virtual machines (HyperKit, Hyper-V)
-        These virtual machines do not have this problem since container to host userId mapping is not the same as Linux
-        Also since some user IDs used in Mac is mapped to reserved user ID in Linux, we just use root by default
+        virtual machines do not have the same problem as Linux since container and host userId is isolated 
+        also since some user IDs used in Mac is mapped to reserved user ID in Linux, we just use root by default
         """
         build_with_logs(
             client=docker_client,
