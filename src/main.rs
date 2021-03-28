c9cee639d8ae53c9a2a40a413f975ae7374d3389
gak <gak@gak0.com>
2021-03-29 05:19:36 +1100

    Revert "limit build time temporarily"
    This reverts commit 14d573ea888fb918d2142930c1e1639fed460e05.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("feeeeeeeless test binary");
+#![forbid(unsafe_code)]
+
+use feeless::cli;
+use tracing::error;
+
+#[tokio::main]
+async fn main() {
+    let result = cli::run().await;
+    if let Err(err) = result {
+        error!("Exiting because of an error: {:?}", err);
+        std::process::exit(1);
+    }
 }
