4437b09cbc88b52008d6cdd953ca4a4cb31254af
gak <gak@gak0.com>
2021-03-28 16:33:58 +1100

    Ci cross (#50)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-#![forbid(unsafe_code)]
-
-use feeless::cli;
-use tracing::error;
-
-#[tokio::main]
-async fn main() {
-    let result = cli::run().await;
-    if let Err(err) = result {
-        error!("Exiting because of an error: {:?}", err);
-        std::process::exit(1);
-    }
+fn main() {
+    println!("feeeeeeeless test binary");
 }
